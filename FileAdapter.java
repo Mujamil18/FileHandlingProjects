802f9098d50c0baa0b6a9d156d878adc2585954f
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2023-02-06 11:35:19 +0530

    Update FileAdapter.java

--- FileAdapter.java
+++ FileAdapter.java
@@ -2,95 +2,163 @@ package com.mydemo.project1;
 
 import java.io.*;
 
-public class FileAdapter
-{
-
+public class FileAdapter {
+    /* created files for User and Admin separately FILEPATH1 for users and FILEPATH2 for admins */
     private static final String FILEPATH1 = "/home/local/ZOHOCORP/mujamil-con1091/Mujamil/Users.txt";
     private static final String FILEPATH2 = "/home/local/ZOHOCORP/mujamil-con1091/Mujamil/Admins.txt";
     File file = null;
+    boolean accountCreated = false;
+    private static String loginName;
 
-    UserDetails userObject = new UserDetails();
-    Users details = userObject.getUserDetails();
-
-    //method to write user details to a file
-    public void writeUserDetails()
-    {
+    /*method to write user details to a file , if the account type is user, that will be stored in Users file
+    and if it is admin , then it will be stored in Admins file*/
+    public void writeUserDetails(Users details) {
         FileWriter fw = null;
-        try
-        {
-            if(details.getAccountType().equals("user"))
-            {
+        try {
+            if (details.getAccountType().equals("user")) {
                 fw = new FileWriter(FILEPATH1, true);
                 fw.write(details.toString());
-            }
-            else if(details.getAccountType().equals("admin"))
-            {
+            } else if (details.getAccountType().equals("admin")) {
                 fw = new FileWriter(FILEPATH2, true);
                 fw.write(details.toString());
-            }
-            else {
+            } else {
                 System.out.println("INVALID ACCOUNT TYPE!!!");
             }
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally
-        {
-            if(fw != null)
-            {
-                try
-                {
+        } finally {
+            if (fw != null) {
+                try {
                     fw.close();
-                }
-                catch(IOException ioe)
-                {
+                } catch (IOException ioe) {
                     ioe.printStackTrace();
                 }
             }
         }
     }
-    //method to write the message to a file
-    public void userPermissions(String recipient)
-    {
-        Messages messageObject = new Messages();
-        writeToFile(messageObject, recipient);
-
-    }
-    //method to get the file path respective to the user
-    public String getFilePath(String name)
-    {
-        return "/home/local/ZOHOCORP/mujamil-con1091/Mujamil/"+name+".txt";
-    }
-    //method to create a file for a registered users
-    public void createUserFile()
-    {
-        String name = details.getName();
-        String userFile = getFilePath(name);
-        try
-        {
-            file = new File(userFile);
-            System.out.println("File Created for you in this path --> "+ userFile);
-            System.out.println("File name --> " + name+".txt" );
+    /*method to read user Details from a file to check Login credentials and will let the user to login
+    only after created account(if the username and password are in the file)*/
+    public boolean readUserDetails(UserLogin login) {
+        String[] registeredUsers;
+        String s;
+        String name = login.getName();
+        String password = login.getPassword();
+        String str = name + " " + password;
+        boolean check = false;
+        FileReader fr = null;
+        try {
+            fr = new FileReader(FILEPATH1);
+            BufferedReader br = new BufferedReader(fr);
+            while ((s = br.readLine()) != null) {
+                registeredUsers = s.split(", ");
+                for (String u : registeredUsers) {
+                    if (str.equals(u)) {
+                        check = true;
+                        break;
+                    }
+                }
+//                System.out.println(Arrays.toString(registeredUsers));
+            }
+                if (check) {
+                    accountCreated = true;
+                    loginName = login.getName();
+                }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fr != null) {
+                try {
+                    fr.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
-        catch(Exception ee)
-        {
+        return accountCreated;
+    }
+    /* method to read the Users file to check whether the recipient is registered or not*/
+    public boolean readRegisteredUsers(String name)
+    {
+        String[] registeredUsers;
+        String s,n="", str;
+        boolean checkRecipient = false;
+        FileReader fr = null;
+        try {
+            fr = new FileReader(FILEPATH1);
+            BufferedReader br = new BufferedReader(fr);
+            while ((s = br.readLine()) != null) {
+                registeredUsers = s.split(", ");
+                for (String u : registeredUsers) {
+                    if (u.contains(name)) {
+                        str = u;
+                        n = "";
+                        for (char i = 0; i < str.length(); i++) {
+                            if (str.charAt(i) == ' ') {
+                                break;
+                            }
+                            n += str.charAt(i);
+                            }
+//                        System.out.println("Name " + n);
+                        if (name.equals(n)) {
+                            checkRecipient = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fr != null) {
+                try {
+                    fr.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return checkRecipient;
+    }
+
+    //method to get the file path respective to the user
+    public String getFilePath(Users details) {
+        return "/home/local/ZOHOCORP/mujamil-con1091/Mujamil/" + details.getName() + ".txt";
+    }
+
+    //method to create a file for a registered users while creating account
+    public void createUserFile(Users details) {
+        String userFile = getFilePath(details);
+        FileWriter fw = null;
+        try {
+            file = new File(userFile);
+            fw = new FileWriter(file);
+            System.out.println("File Created for you in this path --> " + userFile);
+            System.out.println("File name --> " + details.getName() + ".txt");
+            loginName = details.getName();
+            fw.write("THIS IS " + details.getName()+"'s INBOX, YOUR SENT AND RECEIVED MESSAGES WILL APPEAR HERE\n\n");
+
+        } catch (Exception ee) {
             ee.printStackTrace();
         }
+        finally
+        {
+            if (fw != null) {
+                try {
+                    fw.close();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            }
+        }
     }
-//    method to write contents to a file
-    public void writeToFile(Messages msgs, String receive)
+
+    public void writeSentMessages(Messages msgs)
     {
+        String fromFilePath = "/home/local/ZOHOCORP/mujamil-con1091/Mujamil/"+loginName+".txt";
         FileWriter fw = null;
         try
         {
-//            FileOutputStream fos = new FileOutputStream("/home/local/ZOHOCORP/mujamil-con1091/Mujamil/"+receive+".txt");
-//            ObjectOutputStream oos = new ObjectOutputStream(fos);
-//            oos.writeObject(msgs);
-//            oos.close();
-//            System.out.println("The Object  was successfully written to a file");
-            fw = new FileWriter("/home/local/ZOHOCORP/mujamil-con1091/Mujamil/"+receive+".txt");
+            fw = new FileWriter(fromFilePath, true);
             fw.write(msgs.toString());
         }
         catch(Exception e)
@@ -108,5 +176,51 @@ public class FileAdapter
             }
         }
     }
-}
+//    method to write messages to a recipient file, if the recipient is already registered
+    public void writeMessagesToFile(Messages msgs, boolean check) {
+        String toFilePath = "/home/local/ZOHOCORP/mujamil-con1091/Mujamil/" + msgs.getRecipient() + ".txt";
+        FileWriter fw = null;
+        try {
+            if(check) {
+                fw = new FileWriter(toFilePath, true);
+                fw.write(msgs.getMessage() + " From : " + loginName + "\n");
+                System.out.println("Message sent Successfully!");
+            }
+            else {
+                System.out.println("USER DOESN'T EXISTS");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fw != null) {
+                try {
+                    fw.close();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            }
+        }
+    }
 
+    /* method to read message from login account and display */
+    public void readMessageFromFile() {
+        FileReader fr = null;
+        try {
+            fr = new FileReader(file);
+            int i;
+            while ((i = fr.read()) != -1) {
+                System.out.print((char) i);
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } finally {
+            try {
+                if (fr != null) {
+                    fr.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
