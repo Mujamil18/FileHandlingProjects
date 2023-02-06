6233c7ead57698ec3e6b37ffb46be913afee11e9
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2023-02-06 11:38:24 +0530

    Update UserDetails.java

--- UserDetails.java
+++ UserDetails.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class UserDetails
 {
+    Scanner sc = new Scanner(System.in);
     public Users getUserDetails()
     {
-        Scanner sc = new Scanner(System.in);
         System.out.println("Create Account");
         System.out.print("Name --> ");
         String name = sc.nextLine();
@@ -20,5 +20,19 @@ public class UserDetails
         String password = obj.password;
         return new Users(name, email, accountType, password);
     }
-
+    public UserLogin getLoginDetails()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Name --> ");
+        String name= sc.nextLine();
+        System.out.print("Password --> ");
+        String password = sc.nextLine();
+        return new UserLogin(name, password);
+    }
+//    public String searchUser()
+//    {
+//        System.out.print("Enter an USER to search ");
+//        String name = sc.nextLine();
+//        return name;
+//    }
 }
