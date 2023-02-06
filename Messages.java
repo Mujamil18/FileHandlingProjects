a403c24ef523d6a29403470128c8c4c5cebb8438
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2023-02-06 11:36:18 +0530

    Update Messages.java

--- Messages.java
+++ Messages.java
@@ -6,15 +6,28 @@ import java.util.Scanner;
 public class Messages implements Serializable
 {
     String message;
+    String recipient;
     public Messages()
     {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Select a recipient to send a message");
+        recipient = sc.nextLine();
         System.out.println("Type a message");
         message = sc.nextLine();
+
+    }
+
+    public String getRecipient()
+    {
+        return this.recipient;
+    }
+    public String getMessage()
+    {
+        return this.message;
     }
     public String toString()
     {
-        return message;
+        return  "MESSAGE --> " +  message+" SENT TO --> " + recipient+"\n";
     }
 
 }
